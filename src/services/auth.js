ff5b3de049ccc651f52489530c1d25ba932a083c
Wesley_ <wesley.souza@academico.ifpb.edu.br>
2023-01-11 12:59:45 -0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,8 @@
 import { fetchUser } from "./fetchUser"
 
 const BASE_URL = `https://wefitapi.onrender.com/user/login`
+// const BASE_URL = `http://localhost:4000/user/login`
+
 
 export const auth = async (user) => {
     const userAuth = await fetch(BASE_URL, {
